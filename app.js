6ec2cbbebdd618a61df2da45b68127a3fc7f240b
Matis <matischarrier@gmail.com>
2025-10-17 17:17:48 +0200

    fix : cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 const allowedOrigins = [
   'https://coffee-app-two-lyart.vercel.app',
-  'https://la-harde.vercel.app/'
+  'https://la-harde.vercel.app'
 ];
 
 const corsOptions = {
